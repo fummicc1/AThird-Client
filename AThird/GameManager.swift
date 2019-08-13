7c18e5641d3d3c61140d3efd4210da9de740d75f
fummicc1 <fumiya.tennis1@gmail.com>
2019-08-13 17:19:09 +0900

    fix_detached_error

--- AThird/GameManager.swift
+++ AThird/GameManager.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 class Server {
-    static let url: String = "https://athird-swift.herokuapp.com/"
+    static let url: String = "ws://localhost:8080/connectwebsocket"
 }
 
 class GameManager {
